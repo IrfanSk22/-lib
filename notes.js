610001f74b27176043394f7bcbd1de0be141fad3
shaikhirfanali <irfansk.dev@gmail.com>
2024-02-09 12:04:42 +0530

    jquery selectors

---
+++ notes.js
@@ -0,0 +1,17 @@
+/*
+
+WHAT IS JQUERY?
+
+> jQuery is a small, fast and feature-rich JavaScript Library.
+> jQuery makes things like DOM manipulation, event handling and 
+Ajax Much easier
+> Much less code
+
+- SELECTORS
+- EVENTS
+- DOM MANIPULATION
+- METHODS
+- EFFECTS AND ANIMATION
+- AJAX
+
+*/
